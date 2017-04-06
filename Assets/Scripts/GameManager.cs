fdc984acd7c903b9e014dc571ca582c92e49315e
加藤翼 <masiron3939@gmail.com>
2017-04-06 23:55:02 +0900

    修正

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour 
+{
+
+	[SerializeField] private GameObject MarkerObj;
+	private int totalScore;
+	private float distance;
+
+
+	private int CalcScore(float dis)
+	{
+		totalScore += (100 - (int)dis * 30);
+		return totalScore;
+	}
+
+	public void Score (Vector3 hitposition)
+	{
+		distance = Vector2.Distance ((Vector2)hitposition, (Vector2)MarkerObj.transform.position);
+		CalcScore (distance);
+		Debug.Log ("SCORE:" + totalScore);
+	}
+}

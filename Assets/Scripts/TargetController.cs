3efca9c332f673718cfb1266d47e2f2a6f6b86ac
加藤翼 <masiron3939@gmail.com>
2017-04-07 11:46:29 +0900

    修正２

---
+++ Assets/Scripts/TargetController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetController : MonoBehaviour {
+
+
+	[SerializeField] Animator animator;
+	private int DamageCount = 0;
+
+	public void Damaged()
+	{
+		DamageCount++;
+		if (DamageCount >= 5) 
+		{
+			Killed ();
+			Invoke ("Rebirth", 3.0f);
+		}
+	}
+
+	private void Killed ()
+	{
+		animator.SetBool ("broken", true);
+	}
+
+	private void Rebirth ()
+	{
+		DamageCount = 0;
+		animator.SetBool ("broken", false);
+	}
+}
